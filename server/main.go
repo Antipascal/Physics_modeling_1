01e09c94f699401759da1f8c74115e500a3790a9
Antipascal <pesochin_evgeniy@mail.ru>
2022-04-29 16:29:32 +0300

    Patch CORS problem

--- server/main.go
+++ server/main.go
@@ -3,11 +3,16 @@ package main
 import (
 	"net/http"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
+
+	config := cors.DefaultConfig()
+	config.AllowAllOrigins = true
+	r.Use(cors.New(config))
 
 	r.GET("/physics", func(c *gin.Context) {
 		c.JSON(http.StatusOK, "still works")
